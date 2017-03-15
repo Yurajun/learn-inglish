37db7fa2f195281f180473bd9fea0f45a8a2f982
yurajun <yurajun@mail.ru>
2017-03-15 09:44:42 +0300

    First commit

---
+++ js/js.js
@@ -0,0 +1,250 @@
+/* global jq */
+let vocabulary = [];
+
+function updateLocalStorage(val){
+	const obj = JSON.stringify(val);
+	localStorage.setItem('vocabulary', obj);
+}
+
+function mySort(val1, val2){
+	return val1[2].length - val2[2].length;
+}
+
+function getVocabulary() {
+	const library = localStorage.getItem('vocabulary');
+	if (library) {
+		vocabulary = JSON.parse(library);
+	}
+}
+
+const dictionary = localStorage.getItem('vocabulary');
+if (dictionary) {
+	vocabulary = JSON.parse(dictionary);
+}
+
+let textRus;
+let textIng;
+const template = jq('#my-template').html();
+
+function getRandomInt(min, max){
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let wordRus;
+let wordIng;
+let random = 0;
+
+function getRandomWord() {
+	let i = 0;
+	let count = -1;
+	if (vocabulary.length === 0){
+		return;
+	}
+	for (i; i < vocabulary.length; i++){
+		if (vocabulary[i][2] === 'Учу'){
+			count += 1;
+		}
+	}
+	random = getRandomInt(0, count);
+	console.log(random);
+	wordRus = vocabulary[random][0];
+	wordIng = vocabulary[random][1];
+	jq('#text-rus').text(wordRus);
+}
+
+getRandomWord();
+
+function checkResult(){
+	const inputTextIng = document.querySelector('#text-ingl');
+	textIng = jq('#text-ingl').text();
+	const boxMessage = jq('.add-msg');
+	if (textIng === wordIng){
+		boxMessage.html(' ');
+		boxMessage.html('Все верно!!!!!!!').setCss('color', 'green');
+		jq('#text-ingl').text(' ');
+		getRandomWord();
+	}else {
+		boxMessage.html(' ');
+		boxMessage.html('Ответ не верен (').setCss('color', 'red');
+	}
+	inputTextIng.focus();
+}
+
+jq('#text-ingl').keyDown(function (e){
+	if (e.keyCode === 13){
+		checkResult();
+	}
+});
+
+jq('#check').click(function (){
+	checkResult();
+});
+
+function updateTable(){
+	getVocabulary();
+	const tbody = jq('#output');
+	tbody.html(' ');
+
+	function createRow(p) {
+		return template.replace(/{{ID}}/ig, p.id)
+									.replace(/{{RUS}}/ig, p.key)
+									.replace(/{{ING}}/ig, p.val)
+									.replace(/{{KNOW}}/ig, p.know);
+	}
+
+	let i = 0;
+	for (i; i < vocabulary.length; i++){
+		const params = {
+			id: i,
+			key: vocabulary[i][0],
+			val: vocabulary[i][1],
+			know: vocabulary[i][2]
+		};
+		tbody.html(createRow(params));
+	}
+}
+
+updateTable();
+
+function checkWord(word){
+	let i = 0;
+	for (i; i < vocabulary.length; i++){
+		if (vocabulary[i][0] === word ){
+			return true;
+		}
+	}
+}
+
+function updateBlock(){
+	vocabulary.sort(mySort);
+	updateLocalStorage(vocabulary);
+	updateTable();
+	getRandomWord();
+}
+
+jq('#add-word').click(function (){
+	const arr = new Array(3);
+	const boxMessage = jq('.add-msg');
+	textRus = jq('#text-rus').text();
+	textIng = jq('#text-ingl').text();
+	if (textRus !== '' && textIng !== ''){
+		if (checkWord(textRus)){
+			boxMessage.html(' ');
+			boxMessage.html('Такое слово уже есть в словаре').setCss('color', 'silver');
+			return false;
+		}
+		boxMessage.html(' ');
+		arr[0] = textRus;
+		arr[1] = textIng;
+		arr[2] = 'Учу';
+		vocabulary.push(arr);
+		updateBlock();
+		jq('#text-ingl').text(' ');
+		getRandomWord();
+	}else {
+		boxMessage.html(' ');
+		boxMessage.html('Заполни все поля').setCss('color', 'lightgreen');
+	}
+});
+
+function deletWord(key){
+	const templatePopap = jq('#my-template__popap').html();
+	const windowPopap = jq('.window-popap');
+	const body = jq('body');
+	function createWord(word) {
+		return templatePopap.replace(/{{WORD}}/ig, word);
+	}
+	body.setCss('overflow', 'hidden');
+	windowPopap.html(' ');
+	windowPopap.html(createWord(key)).fadeIn(300, function (){
+		jq('.yes-delete').click(function (){
+			windowPopap.fadeOut(300, function (){
+				windowPopap.hide();
+				body.setCss('overflow', 'auto');
+			});
+			let i = 0;
+			for (i; i < vocabulary.length; i++){
+				if (vocabulary[i][0] === key){
+					vocabulary.splice(i, 1);
+				}
+			}
+			updateLocalStorage(vocabulary);
+			updateBlock();
+		});
+		jq('.no-delete').click(function (){
+			windowPopap.fadeOut(300, function (){
+				windowPopap.hide();
+				body.setCss('overflow', 'auto');
+			});
+		});
+	});
+}
+
+jq('#reverse').click(function (){
+	let i = 0;
+	for (i; i < vocabulary.length; i++){
+		if (vocabulary[i][2] === 'Знаю'){
+			vocabulary[i][2] = 'Учу';
+		}else {
+			vocabulary[i][2] = 'Знаю';
+		}
+	}
+	updateBlock();
+});
+
+function knowWord(key){
+	let i = 0;
+	for (i; i < vocabulary.length; i++){
+		if (vocabulary[i][0] === key){
+			if (vocabulary[i][2] === 'Знаю'){
+				vocabulary[i][2] = 'Учу';
+			}else {
+				vocabulary[i][2] = 'Знаю';
+			}
+		}
+	}
+	updateBlock();
+}
+
+jq('#know').click(function (){
+	const txtRus = jq('#text-rus').text();
+	knowWord(txtRus);
+});
+
+jq('#hint').click(function (){
+	const txtRus = jq('#text-rus').text();
+	const boxMessage = jq('.add-msg');
+	let i = 0;
+	let inglWord;
+	for (i; i < vocabulary.length; i++){
+		if (vocabulary[i][0] === txtRus){
+			inglWord = vocabulary[i][1];
+		}
+	}
+	boxMessage.html(' ');
+	boxMessage.html(inglWord).setCss('color', 'orange').fadeIn(500, function (){
+		boxMessage.fadeOut(500, function (){
+			boxMessage.html(' ').setCss('display', 'block').setCss('opacity', 1);
+		});
+	});
+
+});
+
+jq('.table-box').hide();
+
+let flag = true;
+
+jq('#words').click(function (){
+	jq(this).html(' ');
+	if (flag){
+		jq(this).html('Скрыть словарь');
+		jq('.table-box').fadeIn(600);
+		flag = false;
+	}else {
+		jq(this).html('Показать словарь');
+		jq('.table-box').fadeOut(600, function (){
+			jq('.table-box').hide();
+			flag = true;
+		});
+	}
+});
---
+++ js/my-jquery.js
@@ -0,0 +1,144 @@
+function Elems(elems){
+	const elements = elems || [];
+
+	function nospace(str){
+		const VRegExp = new RegExp(/^(\s|\u00A0)+/g);
+		const VResult = str.replace(VRegExp, '');
+		return VResult;
+	}
+
+	this.text = function (text){
+		const txt = text || 'undefined';
+		if (txt === 'undefined'){
+			return nospace(elements[0].value);
+		}
+		let i = 0;
+		for (i; i < elements.length; i++){
+			elements[i].value = txt;
+		}
+		return this;
+	};
+
+	this.html = function (code){
+		if (code === ' '){
+			let i = 0;
+			for (i; i < elements.length; i++){
+				elements[i].innerHTML = code;
+			}
+			return this;
+		}
+		const cods = code || 'undefined';
+		if (cods === 'undefined'){
+			return nospace(elements[0].innerHTML);
+		}
+		let i = 0;
+		for (i; i < elements.length; i++){
+			elements[i].innerHTML += cods;
+		}
+		return this;
+	};
+
+	function fadePrivete(flag, elem, time, callback){
+		let op = 1;
+		const fr = 20;
+		let steps = time / fr;
+		const dOp = op / steps;
+		if (!flag) {
+			op = 0;
+			elem.style.opacity = op;
+			elem.style.display = 'block';
+		}
+		const interval = setInterval(function (){
+			steps -= 1;
+			op = flag ? op - dOp : op + dOp;
+			elem.style.opacity = op;
+			if (steps <= 0){
+				clearInterval(interval); callback();
+			}
+		}, fr);
+	}
+
+	function fade(flag, t, c){
+		const time = t || 300;
+		const callback = c || function (){};
+		let i = 0;
+		for (i; i < elements.length; i++){
+			if (flag){
+				fadePrivete(true, elements[i], time, callback);
+			}else {
+				fadePrivete(false, elements[i], time, callback);
+			}
+		}
+	}
+
+	this.fadeOut = function (t, c){
+		fade(true, t, c);
+		return this;
+	};
+
+	this.fadeIn = function (t, c){
+		fade(false, t, c);
+		return this;
+	};
+
+	this.hide = function (){
+		let i = 0;
+		for (i; i < elements.length; i++){
+			elements[i].style.display = 'none';
+		}
+		return this;
+	};
+
+	this.setCss = function (prop, val){
+		const property = prop || 'undefined';
+		const value = val || 'undefined';
+		if (property === 'undefined' && value === 'undefined'){
+			return this;
+		}
+		let i = 0;
+		for (i; i < elements.length; i++){
+			elements[i].style[property] = value;
+		}
+		return this;
+	};
+
+	function action(event, f){
+		if (f === 'undefined'){
+			let i = 0;
+			for (i; i < elements.length; i++){
+				switch (event){
+					case 'click': elements[i].onclick(); break;
+					case 'enter': elements[i].onkeydown(); break;
+				}
+			}
+		}else {
+			let i = 0;
+			for (i; i < elements.length; i++){
+				switch (event){
+					case 'click': elements[i].onclick = f; break;
+					case 'enter': elements[i].onkeydown = f; break;
+				}
+			}
+		}
+	}
+
+	this.click = function (f){
+		action('click', f);
+		return this;
+	};
+
+	this.keyDown = function (f){
+		action('enter', f);
+		return this;
+	};
+}
+
+function jq(selector){
+	let elems;
+	if (typeof selector === 'string') {
+		elems = document.querySelectorAll(selector);
+	}else {
+		elems = [selector];
+	}
+	return new Elems(elems);
+}
